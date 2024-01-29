e2888a86b96d143d43f695ecc245a477a64d6f42
JyotiDahal123 <jyotirajdahal6@gmail.com>
2024-01-29 02:28:06 -0600

    update

--- src/lib/data.js
+++ src/lib/data.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export async function getAllPosts() {
   try {
-    const res = await axios.get(`http://localhost:3000/api/post`)
+    const res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/post`)
     return res?.data?.allPosts
   } catch (error) {
     throw error.message
@@ -12,7 +12,7 @@ export async function getAllPosts() {
 export async function getSinglePosts(params) {
   try {
     const res = await axios.get(
-      `http://localhost:3000/api/post/${params.slug}`,
+      `${process.env.NEXT_PUBLIC_API_URL}/api/post/${params.slug}`,
     )
 
     return res?.data?.post
