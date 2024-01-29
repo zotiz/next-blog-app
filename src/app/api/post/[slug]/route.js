e0b3eb2244bc00eeab3cdae5129b0d246ff74ce6
JyotiDahal123 <jyotirajdahal6@gmail.com>
2024-01-29 17:39:37 -0600

    update

--- src/app/api/post/[slug]/route.js
+++ src/app/api/post/[slug]/route.js
@@ -1,10 +1,11 @@
+import connectionDB from '@/db.config/db'
 import { PostModel } from '@/model/post.model'
 import { NextResponse } from 'next/server'
-
-export const GET = async (request, {params}) => {
+connectionDB()
+export const GET = async (request, { params }) => {
   try {
-    const {slug} = params
-    const post = await PostModel.findById({ _id:slug })
+    const { slug } = params
+    const post = await PostModel.findById({ _id: slug })
     return NextResponse.json({ message: 'success', post })
   } catch (error) {
     return NextResponse.json({ message: error.message })
